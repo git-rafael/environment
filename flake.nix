ace3eae1a7d05c17ffc9bd10dfffbf9041bd6b9f
Rafael Oliveira <rafael@redeoliveira.com>
2022-05-25 19:46:43 -0300

    -

--- flake.nix
+++ flake.nix
@@ -26,7 +26,7 @@
       configuration.imports = modulePaths;
     };
 
-    mkContainerDerivation = { nixpkgs, home-manager, ... }: modulePath: x86_64-linuxPkgs.dockerTools.buildImage (x86_64-linuxPkgs.callPackage modulePath { inherit nixpkgs home-manager; });
+    mkContainerDerivation = { nixpkgs, home-manager, ... }: modulePath: x86_64-linuxPkgs.dockerTools.buildImage (import modulePath { inherit nixpkgs home-manager; });
 
   in {
     nixOnDroidConfigurations.phone = mkDeviceMobileDerivation "aarch64-linux" [

a5524257cee572111114f76816291e81cd233564
Rafael Oliveira <rafael@redeoliveira.com>
2022-05-26 18:46:40 -0300

    -

--- modules/profiles/science.nix
+++ modules/profiles/science.nix
@@ -25,7 +25,7 @@ let
           sha256 = "sha256-PoiOWb9XJ3y9bzg8sjKFiy18cSGeV0klcSjxbZhX5Gw=";
         };
         buildInputs = with super;
-          [ six sqlalchemy self.prettytable ipython_genutils ipython ];
+          [ six sqlalchemy sqlparse self.prettytable ipython_genutils ipython ];
       };
 
       jupyter_http_over_ws = super.buildPythonPackage rec {

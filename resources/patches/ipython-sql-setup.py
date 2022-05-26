9266f77f8926677159d3fd6bce3e303540610148
Rafael Oliveira <rafael@redeoliveira.com>
2022-05-26 17:22:37 -0300

    -

--- resources/patches/ipython-sql-setup.py
+++ resources/patches/ipython-sql-setup.py
@@ -1,16 +1,15 @@
 diff --git a/setup.py b/setup.py
-index 354cdab..22cc24e 100644
+index e4a653d..70e98ad 100644
 --- a/setup.py
 +++ b/setup.py
-@@ -1,9 +1,10 @@
- import os
+@@ -1,8 +1,9 @@
  from io import open
-+from pathlib import
- 
- from setuptools import find_packages, setup
+ from setuptools import setup, find_packages
+ import os
++from pathlib import Path
  
 -here = os.path.abspath(os.path.dirname(__file__))
 +here = Path(__file__).parent.resolve()
- README = open(os.path.join(here, "README.rst"), encoding="utf-8").read()
- NEWS = open(os.path.join(here, "NEWS.rst"), encoding="utf-8").read()
+ README = open(os.path.join(here, 'README.rst'), encoding='utf-8').read()
+ NEWS = open(os.path.join(here, 'NEWS.rst'), encoding='utf-8').read()
  

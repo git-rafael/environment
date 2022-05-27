0aad03528b6a4d0a2a92eb7a2e813a208f170605
Rafael Oliveira <rafael@redeoliveira.com>
2022-05-26 22:55:18 -0300

    -

--- modules/profiles/science.nix
+++ modules/profiles/science.nix
@@ -4,5 +4,11 @@ let
   sciencePackages = import ../packages/science.nix pkgs;
 
 in {
-  home.packages = sciencePackages.withIpython;
+  # home.packages = sciencePackages.withIpython;
+
+  home.packages = pkgs.buildFHSUserEnv {
+    name = "pipzone";
+    targetPkgs = pkgs: sciencePackages.withIpython;
+    runScript = "bash";
+  };
 }

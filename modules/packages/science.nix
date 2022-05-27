85466d2ceead899ce40ab920507ab8031dda62a9
Rafael Oliveira <rafael@redeoliveira.com>
2022-05-26 23:33:37 -0300

    -

--- modules/packages/science.nix
+++ modules/packages/science.nix
@@ -42,7 +42,7 @@ pkgs: with pkgs;
             sha256 = "sha256-H+mt0swUhRJG4VJ8jYeFFPfPVRHQW/EXb302xMtDp2M=";
           };
           buildInputs = with super;
-            [ python-dateutil pyzmq rich ];
+            [ rich ];
           propagatedBuildInputs = with super;
             [ python-dateutil pyzmq ];
         };
